09031e4c67fe42c82938a4fc9d324fb12b61054c
dklassen <dana.klassen@shopify.com>
2015-12-29 10:16:41 -0500

    Use existing bonjour library

--- serve.go
+++ serve.go
@@ -2,23 +2,36 @@ package main
 
 import (
 	"flag"
-	"github.com/joelgibson/go-airplay/airplay"
 	"log"
 	"net"
 	"os"
+
+	"github.com/joelgibson/go-airplay/airplay"
 )
 
 func main() {
 	debug := flag.Bool("debug", false, "Show debug output.")
-	name := flag.String("name", "Swaggatron", "AirTunes name.")
+	name := flag.String("name", "AirSonos", "AirTunes name.")
 	flag.Parse()
 
 	if *debug {
 		airplay.Debug = log.New(os.Stderr, "DEBUG ", log.LstdFlags)
 	}
-	err := airplay.ServeAirTunes(*name, func(id string, conn net.Conn) {
+
+	address := ":49152"
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Panic("Listen failed: %s", err)
+	}
+	defer listener.Close()
+
+	bonjourServer, err := airplay.RegisterAirTunes(*name, address)
+	defer bonjourServer.Shutdown()
+
+	err = airplay.ServeAirTunes(*name, address, listener, func(id string, conn net.Conn) {
 		airplay.RtspSession(id, conn, func(x chan string) {})
 	})
+
 	if err != nil {
 		log.Fatal(err)
 	}
